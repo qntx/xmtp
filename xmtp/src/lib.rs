174fa82055bc5a9fb06f71125ad8714266f3c9dc
x.qntx.eth <x@qntx.fun>
2026-04-23 20:21:11 +0800

    refactor(docs): split doc_auto_cfg into separate doc_cfg feature and auto_cfg attribute
    - Replace combined `doc_auto_cfg` feature with `doc_cfg` feature gate
    - Add separate `doc(auto_cfg)` attribute for automatic cfg documentation
    - Aligns with Rust nightly API changes where doc_auto_cfg was split into two parts

--- xmtp/src/lib.rs
+++ xmtp/src/lib.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, doc(auto_cfg))]
 #![doc = include_str!("../README.md")]
 #![allow(
     unsafe_code,
